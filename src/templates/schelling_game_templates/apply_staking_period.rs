7f149d6132e1f7a6196a34faed546a6f43f252f2
Amiya Behera <amiyatulu@gmail.com>
2024-04-20 16:45:23 +0530

    apply staking period

---
+++ src/templates/schelling_game_templates/apply_staking_period.rs
@@ -0,0 +1,64 @@
+use crate::components::schelling_game::{{module_name}}::apply_staking_period_sign_in::SignTransaction;
+use crate::components::schelling_game::{{module_name}}::storage::get_period::GetPeriod;
+use crate::components::schelling_game::{{module_name}}::change_period::ChangePeriod;
+use crate::services::common_imp::View;
+use crate::services::error::ErrorString;
+use leptos::ev::SubmitEvent;
+use leptos::*;
+
+#[component]
+pub fn ApplyStakingPeriod({{params_variable}}: {{params_variable_type}}) -> impl IntoView {
+
+    // gloo::console::log!({{params_variable}}());
+    let (current_view, set_current_view) = create_signal(View::Form);
+    let submit_click = move |e: SubmitEvent| {
+        e.prevent_default();
+
+        set_current_view(View::Success);
+    };
+
+
+    let render_view = move || match current_view() {
+        View::Form => {
+            view! {
+                <div
+                class="max-w-5xl mx-auto max-md:mx-10"
+                >
+                <GetPeriod {{params_variable}}={{params_variable}}.clone() /> 
+                <ChangePeriod {{params_variable}}={{params_variable}}.clone() />
+                    <form
+                        
+                        id="apply-staking-period-from"
+                        on:submit=submit_click
+                    >
+                        <div>Apply Staking Period</div>
+                        <button
+                            type="submit"
+                            id="apply-juror-submit"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            }
+        }
+        View::Success => {
+            view! {
+                <div>
+                    <SignTransaction
+                        {{params_variable}}={{params_variable}}.clone()
+                    />
+
+                </div>
+            }
+        }
+    };
+
+    view! {
+        <>
+            {move || render_view()}
+        </>
+    }
+}
---
+++ src/templates/schelling_game_templates/apply_staking_period_sign_in.rs
@@ -0,0 +1,94 @@
+use crate::components::transaction::extension_sign_in::sign_in_with_extension;
+use crate::components::transaction::get_accounts_extension::GetAccountsExtension;
+use crate::services::common_services::polkadot;
+use leptos::*;
+use std::str::FromStr;
+use subxt::utils::AccountId32;
+
+#[component]
+pub fn SignTransaction({{params_variable}}: {{params_variable_type}}) -> impl IntoView {
+    view! { <ExtensionSignIn {{params_variable}}={{params_variable}}/> }
+}
+
+#[component]
+pub fn ExtensionSignIn({{params_variable}}: {{params_variable_type}}) -> impl IntoView {
+    let (account_load, set_account_load) = create_signal(("".to_owned(), "".to_owned()));
+
+    let render_html = move || {
+        if account_load().0.is_empty() || account_load().1.is_empty() {
+            view! {
+                <div>
+                    <GetAccountsExtension set_account_load=set_account_load/>
+                </div>
+            }
+        } else if !account_load().0.is_empty() && !account_load().1.is_empty() {
+            view! {
+                <div>
+                    <ExtensionTransaction
+                        {{params_variable}}={{params_variable}}.clone()
+                        account_address=account_load().0
+                        account_source=account_load().1
+                    />
+                </div>
+            }
+        } else {
+            view! { <div>{"Some Error Occured"}</div> }
+        }
+    };
+
+    view! { <div>{move || render_html()}</div> }
+}
+
+#[component]
+pub fn ExtensionTransaction(
+    {{params_variable}}: {{params_variable_type}},
+    account_address: String,
+    account_source: String,
+) -> impl IntoView {
+    let (error, set_error) = create_signal(String::from(""));
+    let (extrinsic_success, set_extrinsic_success) = create_signal(String::from(""));
+    let transaction_resource = create_local_resource(
+        move || {
+            (
+                {{params_variable}}.clone(),
+                account_address.clone(),
+                account_source.clone(),
+                set_error,
+                set_extrinsic_success,
+            )
+        },
+        move |(
+            {{params_variable}},
+            account_address,
+            account_source,
+            set_error,
+            set_extrinsic_success,
+        )| async move {
+
+            {% if params_type is containing("account") %}
+            let account_id32 = AccountId32::from_str(&{{params_variable}}.clone()).unwrap();
+
+            let tx = polkadot::tx()
+                .{{template_function_name}}()
+                .apply_staking_period(account_id32);
+            {% endif %}
+
+            {% if params_type is containing("number") %}
+            let tx = polkadot::tx()
+                .{{template_function_name}}()
+                .apply_staking_period({{params_variable}});
+            {% endif %}
+
+            sign_in_with_extension(
+                tx,
+                account_address,
+                account_source,
+                set_error,
+                set_extrinsic_success,
+            )
+            .await;
+        },
+    );
+
+    {{extrinsic_load}}
+}
